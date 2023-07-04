41544b4af7d511c6f2fc9681d1e9089c1185c492
IrynaChebotar <iryna.chebotar2@gmail.com>
2023-07-04 21:26:49 +0200

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup =
+    galleryItems.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    }).join('');
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+function openImg(event){
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG"){
+        return;
+    }
+        const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`)
+
+instance.show()
+    
+}
+
+galleryEl.addEventListener("click", openImg);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup =
+    galleryItems.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    }).join('');
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+galleryEl.addEventListener("click", openImg);
+
+function openImg(event){
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG"){
+        return;
+    }
+    let gallery =  new SimpleLightbox('.gallery a', {
+                captionsData: 'alt',
+                captionDelay: 250,
+             }); 
+    
+}
+
+
+
+
+
+
